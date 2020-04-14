2926d68bbe98239a0a56624ab5dd74fdd3549b08
Nate Ijams <exprezdev@pm.me>
2020-04-14 16:16:38 -0500

    Travis for windows 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 shopt -s nullglob
 cd app/dist/
-for f in *.{dmg,zip,deb,rpm}; do
+for f in *.{dmg,zip,deb,rpm,exe}; do
   curl -F "fileToUpload=@$f" -F 'reqtype=fileupload' 'https://catbox.moe/user/api.php'
   printf "\n"
 done
